0ab073f33def5e2cc0ab13ce1ba9224f88d7db61
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2024-07-24 18:47:19 -0400

    add tsconfig.json and another example

---
+++ intro/noImplicitAny_ex.ts
@@ -0,0 +1,9 @@
+// with "noImplicitAny": false in tsconfig.json, anys: any[]
+const anys = [];
+anys.push(1);
+anys.push("oh no");
+anys.push({ anything: "goes" });
+
+let message2 = anys[2]
+
+console.log(message2)
